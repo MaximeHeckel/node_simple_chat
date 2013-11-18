b6f18326abcc066bb48f23d59f39f29cff0d9c45
Maxime Heckel <heckelmaxime@gmail.com>
2013-11-18 20:29:27 +0100

    Added multi client support

--- chatserver.js
+++ chatserver.js
@@ -1,11 +1,15 @@
 var net = require('net');
 
 var chatServer = net.createServer();
+var clientList = [];
 
 chatServer.on('connection', function(client){
   client.write('Hi!\n');
+  clientList.push(client);
   client.on('data', function(data){
-    console.log(data);
+    for(var i = 0; i<clientList.length; i+=1){
+      clientList[i].write(data);
+    }
   })
 });
 
